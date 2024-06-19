4585fc3e7b63f57692c0bfd8e548558e8ff4acd3
CWood-sdf <woodchristophermichael@gmail.com>
2024-06-19 19:42:36 -0400

    fix: actually find plugin dir

--- build.lua
+++ build.lua
@@ -1,15 +1,11 @@
 vim.schedule(function()
-    local file = io.open("BANANA_YEET_BANANA.txt", "w")
-    file:write("asdf\n")
     local pluginCwd = ""
 
     for _, v in ipairs(vim.api.nvim_list_runtime_paths()) do
-        file:write(v .. ", " .. pluginCwd .. "\n")
-        if v:match("banana.nvim(/|)$") then
+        if v:match("banana.nvim$") ~= nil then
             pluginCwd = v
         end
     end
-    file:close()
     vim.system({ "bash", "./build.sh" }, {
         cwd = pluginCwd
     }):wait()
