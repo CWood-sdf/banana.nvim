373e3b7e65ca00f63d4738d3bbd5f5ea7e848e64
CWood-sdf <woodchristophermichael@gmail.com>
2024-06-19 19:40:33 -0400

    feat: add cwd find

--- build.lua
+++ build.lua
@@ -1,6 +1,16 @@
 vim.schedule(function()
     local file = io.open("BANANA_YEET_BANANA.txt", "w")
-    file:write("asdf")
+    file:write("asdf\n")
+    local pluginCwd = ""
+
+    for _, v in ipairs(vim.api.nvim_list_runtime_paths()) do
+        file:write(v .. ", " .. pluginCwd .. "\n")
+        if v:match("banana.nvim(/|)$") then
+            pluginCwd = v
+        end
+    end
     file:close()
-    vim.system({ "bash", "./build.sh" }, {}):wait()
+    vim.system({ "bash", "./build.sh" }, {
+        cwd = pluginCwd
+    }):wait()
 end)
