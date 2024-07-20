3c1aaa78fc9c5ff2ae1c5b1d85fc570d51d3494e
Christopher Wood <woodchristophermichael@gmail.com>
2024-07-19 22:37:26 -0400

    feat(instance): dont render main in createWinAndBuf

--- lua/banana/instance.lua
+++ lua/banana/instance.lua
@@ -68,6 +68,7 @@ function Instance:virtualRender(ast, width, height)
         local extra = {
             trace = require('banana.box').Box:new(),
             debug = self.DEBUG,
+            head = false,
         }
         local rendered = tag:renderRoot(ast, nil, width, height, {
             text_align = "left",
@@ -424,6 +425,7 @@ function Instance:createWinAndBuf()
         }, {
             trace = require('banana.box').Box:new(),
             debug = self.DEBUG,
+            head = true,
         })
     end
     local containerWidth = vim.o.columns
