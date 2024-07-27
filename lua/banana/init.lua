326792235cb4a01fc3ab5b6d596bdb2bb3ff89b0
Rohan Seth <rohanseth2006@gmail.com>
2024-07-27 16:30:36 -0700

    fix(treesitter): register nml & ncss filetypes

--- lua/banana/init.lua
+++ lua/banana/init.lua
@@ -9,100 +9,104 @@ local tsInit = false
 local tsInstall = false
 
 M.runCounter = function()
-	if counterInst == nil then
-		counterInst = render.newInstance("examples/counter", "asdf")
-		-- instance.DEBUG = true
-	end
-	counterInst:open()
+    if counterInst == nil then
+        counterInst = render.newInstance("examples/counter", "asdf")
+        -- instance.DEBUG = true
+    end
+    counterInst:open()
 end
 M.runTodo = function()
-	if todoInst == nil then
-		todoInst = render.newInstance("examples/todo", "asdf")
-		-- instance.DEBUG = true
-	end
-	todoInst:open()
+    if todoInst == nil then
+        todoInst = render.newInstance("examples/todo", "asdf")
+        -- instance.DEBUG = true
+    end
+    todoInst:open()
 end
 
 M.runLazy = function()
-	if instance == nil then
-		instance = render.newInstance("examples/lazy", "")
-		-- instance.DEBUG = true
-		-- instance.DEBUG_showPerf = true
-		-- instance.DEBUG_stressTest = true
-	end
-	instance:open()
-	instance:_requestRender()
+    if instance == nil then
+        instance = render.newInstance("examples/lazy", "")
+        -- instance.DEBUG = true
+        -- instance.DEBUG_showPerf = true
+        -- instance.DEBUG_stressTest = true
+    end
+    instance:open()
+    instance:_requestRender()
 end
 
 function M.spam()
-	local testFile = [[
+    local testFile = [[
 			.asdf {
 				hl-bg: #0000ff;
 			}
 		]]
-	local startTime = vim.uv.hrtime()
-	for _ = 1, 1000 do
-		local _ = require('banana.ncss.parser').parseTextSlow(testFile)
-	end
-	vim.notify((vim.uv.hrtime() - startTime) / 1e6 .. "ms\n")
-	startTime = vim.uv.hrtime()
-	for _ = 1, 1000 do
-		local _ = require('banana.ncss.parser').parseText(testFile)
-	end
-	vim.notify((vim.uv.hrtime() - startTime) / 1e6 .. "ms\n")
+    local startTime = vim.uv.hrtime()
+    for _ = 1, 1000 do
+        local _ = require('banana.ncss.parser').parseTextSlow(testFile)
+    end
+    vim.notify((vim.uv.hrtime() - startTime) / 1e6 .. "ms\n")
+    startTime = vim.uv.hrtime()
+    for _ = 1, 1000 do
+        local _ = require('banana.ncss.parser').parseText(testFile)
+    end
+    vim.notify((vim.uv.hrtime() - startTime) / 1e6 .. "ms\n")
 end
 
 function M.getInstallDir()
-	local ret = ""
+    local ret = ""
 
-	for _, v in ipairs(vim.api.nvim_list_runtime_paths()) do
-		if v:match("banana.nvim$") ~= nil then
-			ret = v
-		end
-	end
-	return ret
+    for _, v in ipairs(vim.api.nvim_list_runtime_paths()) do
+        if v:match("banana.nvim$") ~= nil then
+            ret = v
+        end
+    end
+    return ret
 end
 
 function M.installTsParsers()
-	if tsInstall then
-		return
-	end
-	M.initTsParsers()
-	tsInstall = true
-	vim.cmd("TSInstallSync nml")
-	vim.cmd("TSInstallSync ncss")
+    if tsInstall then
+        return
+    end
+    M.initTsParsers()
+    tsInstall = true
+    vim.cmd("TSInstallSync nml")
+    vim.cmd("TSInstallSync ncss")
 end
 
 function M.initTsParsers()
-	if tsInit then
-		return
-	end
-	tsInit = true
-	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-	vim.treesitter.language.register("nml", "nml")
-	---@diagnostic disable-next-line: inject-field
-	parser_config.nml = {
-		install_info = {
-			url = M.getInstallDir() .. "/tree-sitter-nml",
-			files = { "src/parser.c" },
-			branch = "main",
-			generate_requires_npm = false,
-			requires_generate_from_grammar = false,
-		},
-		filetype = "nml",
-	}
-	---@diagnostic disable-next-line: inject-field
-	parser_config.ncss = {
-		install_info = {
-			url = M.getInstallDir() .. "/tree-sitter-ncss",
-			files = { "src/parser.c", "src/scanner.c" },
-			branch = "main",
-			generate_requires_npm = false,
-			requires_generate_from_grammar = false,
-		},
-		filetype = "ncss",
-	}
-	vim.treesitter.language.register("ncss", "ncss")
+    if tsInit then
+        return
+    end
+    tsInit = true
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    vim.filetype.add({ extension = { nml = "nml" } })
+    vim.treesitter.language.register("nml", "nml")
+    ---@diagnostic disable-next-line: inject-field
+    parser_config.nml = {
+        install_info = {
+            url = M.getInstallDir() .. "/tree-sitter-nml",
+            files = { "src/parser.c" },
+            branch = "main",
+            generate_requires_npm = false,
+            requires_generate_from_grammar = false,
+        },
+        filetype = "nml",
+    }
+
+    vim.filetype.add({ extension = { ncss = "ncss" } })
+    vim.treesitter.language.register("ncss", "ncss")
+    ---@diagnostic disable-next-line: inject-field
+    parser_config.ncss = {
+        install_info = {
+            url = M.getInstallDir() .. "/tree-sitter-ncss",
+            files = { "src/parser.c", "src/scanner.c" },
+            branch = "main",
+            generate_requires_npm = false,
+            requires_generate_from_grammar = false,
+        },
+        filetype = "ncss",
+    }
 end
 
 return M
