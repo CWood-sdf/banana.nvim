0095dfaa5b51e4af348e234e5c07e890f6a63a2a
Christopher Wood <woodchristophermichael@gmail.com>
2024-08-21 21:38:17 -0400

    test: maybe they work now

--- build.lua
+++ build.lua
@@ -1,13 +1,13 @@
-vim.schedule(function ()
-    local job = vim.fn.jobstart("zig build -Doptimize=ReleaseFast",
-        {
-            cwd = "./zig",
-        })
+-- vim.schedule(function ()
+local job = vim.fn.jobstart("zig build -Doptimize=ReleaseFast",
+    {
+        cwd = "./zig",
+    })
 
-    vim.fn.jobwait({ job })
+vim.fn.jobwait({ job })
 
-    -- require('banana').initTsParsers()
-    -- vim.cmd("TSInstallSync nml")
-    -- vim.cmd("TSInstallSync ncss")
-    -- vim.cmd("qa!")
-end)
+-- require('banana').initTsParsers()
+-- vim.cmd("TSInstallSync nml")
+-- vim.cmd("TSInstallSync ncss")
+-- vim.cmd("qa!")
+-- end)
