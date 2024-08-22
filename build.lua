a9fbbce4fbc7b62a6a7e9089dbe5515b1a448e64
Christopher Wood <woodchristophermichael@gmail.com>
2024-08-21 21:44:11 -0400

    test: more test logging

--- build.lua
+++ build.lua
@@ -5,6 +5,22 @@ local job = vim.fn.jobstart("zig build -Doptimize=ReleaseFast",
         detach = true,
         on_exit = function (p1, p2, p3)
             print("Exit parameters: " .. p1 .. ", " .. p2 .. ", " .. p3)
+        end,
+        on_stderr = function (p1, p2, p3)
+            p1 = p1 or ""
+            p2 = p2 or ""
+            p3 = p3 or ""
+            print("Stderr params: " ..
+                vim.inspect(p1) ..
+                ", " .. vim.inspect(p2) .. ", " .. vim.inspect(p3))
+        end,
+        on_stdout = function (p1, p2, p3)
+            p1 = p1 or ""
+            p2 = p2 or ""
+            p3 = p3 or ""
+            print("stdout params: " ..
+                vim.inspect(p1) ..
+                ", " .. vim.inspect(p2) .. ", " .. vim.inspect(p3))
         end
     })
 
