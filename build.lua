60b8c806ec9c19d91c00580dd4947583410bda0b
Christopher Wood <woodchristophermichael@gmail.com>
2024-08-22 23:49:20 -0400

    chore: add notify of cwd if wrong

--- build.lua
+++ build.lua
@@ -1,6 +1,7 @@
 local cwd = vim.fn.getcwd()
 
 if cwd:match("banana.nvim$") == nil then
+    vim.notify(cwd)
     error("Expected build.lua to be run with banana install dir as the cwd")
 end
 vim.opt.runtimepath:append(cwd)
