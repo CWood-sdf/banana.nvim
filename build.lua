9b9b3ced453fb91f3a5eeec2c32bbb8490f44783
Christopher Wood <woodchristophermichael@gmail.com>
2024-08-22 23:50:41 -0400

    chore: add cwd update to build

--- build.lua
+++ build.lua
@@ -1,8 +1,12 @@
 local cwd = vim.fn.getcwd()
 
 if cwd:match("banana.nvim$") == nil then
-    vim.notify(cwd)
-    error("Expected build.lua to be run with banana install dir as the cwd")
+    if vim.fn.isdirectory(cwd .. "/banana.nvim") then
+        cwd = cwd .. "banana.nvim"
+    else
+        vim.notify(cwd)
+        error("Expected build.lua to be run with banana install dir as the cwd")
+    end
 end
 vim.opt.runtimepath:append(cwd)
 require("banana").installLibbanana()
