ffdc644b1ca6659f5d9ecb5775fee2b9a407bb86
Christopher Wood <woodchristophermichael@gmail.com>
2024-12-26 22:56:05 -0500

    ci(doc): idk

--- gendoc/gendoc.sh
+++ gendoc/gendoc.sh
@@ -4,5 +4,7 @@ newname=$(echo $1)
 
 echo "Documenting $newname"
 
+echo "nvimdoc/panvimdoc.sh --input-file $1 --project-name banana-$1 --scripts-dir ../panvimdoc/scripts"
+
 ../panvimdoc/panvimdoc.sh --input-file $1 --project-name banana-$1 --scripts-dir ../panvimdoc/scripts || echo "Failed"
 
