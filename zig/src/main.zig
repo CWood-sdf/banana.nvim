8a4df497b70bff51e23d22aa748526d7949dc7e6
Christopher Wood <woodchristophermichael@gmail.com>
2025-05-14 00:18:57 -0400

    feat(zig): testing for simdz Char

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -2,11 +2,43 @@ const std = @import("std");
 const box = @import("box.zig");
 const gen = @import("genlua.zig");
 const hl = @import("hl.zig");
+const Char = box.Char;
 
 pub fn main() !void {
-    const vec: @Vector(4, u8) = std.simd.iota(u8, 4) + @as(@Vector(4, u8), @splat(0b1000_0000));
-    const v2 = @reduce(.And, vec);
-    std.debug.print("{}\n", .{v2});
+    const chars = "asdfasdf";
+    const charsVec: @Vector(Char.simdLen, u8) = chars[0..8].*;
+    const simd = Char.simdToChars(charsVec);
+    const charArr: [8]Char = @bitCast(simd);
+
+    const bitsBacking: u16 = 0b1111_0000_0101_1010;
+    const bits: @Vector(2, u8) = @bitCast(bitsBacking);
+
+    const newBits: u16 = @bitCast(@bitReverse(bits));
+    std.debug.print("bits: {b:0>16}\n", .{newBits});
+
+    const char0: Char = @bitCast(charArr[0]);
+
+    const realChar = Char.fromAscii('a');
+
+    var charSlice = [_]u8{0} ** 16;
+    var i: usize = 0;
+    for (charArr) |c| {
+        const width = c.toBytes(charSlice[i..]) catch {
+            std.debug.print("big sad :( {}\n", .{i});
+            break;
+        };
+        i += width;
+    }
+    std.debug.print("New str: {s}\n", .{charSlice[0..i]});
+    std.debug.print("ordering: {s}\n", .{@tagName(Char.ordering)});
+
+    std.debug.print("width: {}\n", .{char0.width});
+    std.debug.print("bytes: {}\n", .{char0.bytes});
+    std.debug.print("char 0: {b:0>8}\n", .{'a'});
+    std.debug.print("real  : {b:0>32}\n", .{@as(u32, @bitCast(realChar))});
+    std.debug.print("actual: {b:0>32}\n", .{@as(u32, @bitCast(char0))});
+    // std.debug.print("char 0: {b:0>21}\n", .{char0.char});
+    // std.debug.print("char 0: {b:0>8}\n", .{@bitReverse(@as(u8, @intCast(@bitReverse(char0.char))))});
 }
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
